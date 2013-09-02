2788d57d356642becb001a88f687c099985d1214
Ian Li <i@techotaku.net>
2013-09-02 17:16:26 +0800

    Add config file for test

---
+++ test/testcfg.php
@@ -0,0 +1,17 @@
+<?php
+/**
+  * Configuration
+  */
+
+// Test constant
+define('APP_URL_ROOT', 'http://127.0.0.1:8080');
+define('DOUBAN_API', 'https://127.0.0.1:8081');
+
+// define your token (mock)
+define('TOKEN', 'dbcha.test.token');
+define('DEBUG', FALSE);
+
+// Douban app public key (mock)
+define('DKEY', 'dbcha.mock.key');
+// Douban app secret key (mock)
+define('DSERCRET', 'dbcha.mock.secret');
